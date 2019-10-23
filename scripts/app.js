ca01fec2b224b24f58364888f1958e9383f8e0c8
Michael Frantz <mjfrantz26@gmail.com>
2019-10-23 22:58:32 +0900

    Added Navbar & Used click event for search bar

--- scripts/app.js
+++ scripts/app.js
@@ -13,7 +13,7 @@ function getCatalog () {
         },
         {
             title:"Second Item",
-            description:"Random description",
+            description:"fun description",
             price: 110.99,
             image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvb_a0JxBfA1StsV-2eIeAAtp7an-Qig9H0aU1-TK20MdtLW3viA",
             category:"shortBoard"
@@ -22,34 +22,61 @@ function getCatalog () {
 }
 
 function displayCatalog () {
-    //get the reference to the UL
-    var ul = $("#catalog");
-
     //for every item on the catalog array,
     for(let i=0; i<catalog.length; i++){
         var item = catalog[i];
-        
-        //create an LI
-        var li = 
-        `<div class="card" style="width: 18rem;">
-            <img src="${item.image}" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">${item.title}</h5>
-                <p class="card-text">${item.description}</p>
-                <a href="#" class="btn btn-primary">Add to cart</a>
-                <h6>${item.price}</h6>
-             </div>
-        </div>`;
-        //add the li to UL
-        ul.append(li);
+        displayItem(item);
      }
+}
+
+function displayItem(item){
+    //get the reference to ul
+    var ul = $("#catalog");
+
+    //create an LI
+    var li = 
+    `<div class="card" style="width: 18rem;">
+        <img src="${item.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${item.title}</h5>
+            <p class="card-text">${item.description}</p>
+            <h6>${item.price}</h6>
+            <a href="#" class="btn btn-primary">Add to cart</a>
+         </div>
+    </div>`;
+    //add the li to UL
+    ul.append(li);
+}
+
+function search () {
+    var text = $("#txtSearch").val().toLowerCase();
+
+    $("#catalog").html(' ');
+    for(let i =0; i < catalog.length; i++){
+        var item = catalog[i];
+
+        if(item.title.toLowerCase().indexOf(text) >= 0
+        || item.description.toLowerCase().indexOf(text) >= 0){
+            displayItem(item);
+        }
+    }
 }
 
 function init() {
     console.log("hi");
-
+    // initalizing 
     getCatalog();
     displayCatalog();
+    //events 
+    $("#btnSearch").click(search);
+    $("#txtSearch").keypress(function(arg){
+        if(arg.keyCode == 13) {
+           search();
+
+            arg.preventDefault(); //prevent page to refresh
+        }
+    })
+    
 }
 
 window.onload = init;
